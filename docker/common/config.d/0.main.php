1ea95ca27caf87c05369f8dba24ecce0ce66240b
erleibiazzio <erleibiazzio@gmail.com>
2025-05-06 17:39:37 -0300

    Configura thema themeCulturaenlinea

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -5,7 +5,7 @@ return [
     'app.siteDescription' => '',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
-    'themes.active' => 'MapasCulturais\Themes\BaseV2',
+    'themes.active' => 'themeCulturaenlinea',
 
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
