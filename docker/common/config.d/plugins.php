731b5290a4d3993db8cd843013eb906dabaa155f
erleibiazzio <erleibiazzio@gmail.com>
2025-05-07 12:03:41 -0300

    Instala plugins que auxiliam analises e debugs no ambiente

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -4,5 +4,15 @@ return [
     'plugins' => [
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        'AdminLoginAsUser',
+        'SpamDetector',
+        'MapasBlame' => [
+            'namespace' => 'MapasBlame',
+            'config' => [
+                'request.logData.PATCH' => function ($data) {
+                    return $data;
+                },
+            ]
+        ],
     ]
 ];
