65d8856a57190e22ad7a8e68a89607726eca5053
Enrique Amestoy <eamestoy@users.noreply.github.com>
2025-05-14 12:29:09 -0300

    Create uruguay.php

---
+++ hardcode/uruguay.php
@@ -0,0 +1,141 @@
+<?php
+
+$data = [
+    'AR' => [
+        'label' => 'Artigas',
+        'cities' => [
+            'Artigas', 'Bella Unión', 'Tomás Gomensoro', 'Baltasar Brum', 'Sequeira', 'Franquía',
+            'Cuareim', 'Mones Quintela', 'Colonia Palma', 'Coronado', 'Bernabé Rivera', 'Cainsa',
+            'Portones de Hierro y Campodónico', 'Paso Campamento', 'Javier de Viana', 'Diego Lamas',
+            'Topador', 'Cuaró', 'Paso Farías', 'Rincón de Pacheco'
+        ]
+    ],
+    'CA' => [
+        'label' => 'Canelones',
+        'cities' => [
+            '18 de Mayo', 'Aguas Corrientes', 'Atlántida', 'Barros Blancos', 'Casarino', 'Canelones',
+            'Ciudad de la Costa', 'Colonia Nicolich', 'Empalme Olmos', 'Estación Atlántida',
+            'Estación La Floresta', 'Joaquín Suárez', 'Juanicó', 'La Floresta', 'La Paz', 'Las Piedras',
+            'Las Toscas', 'Los Cerrillos', 'Marindia', 'Migues', 'Montes', 'Neptunia', 'Pando',
+            'Parque del Plata', 'Paso Carrasco', 'Pinamar - Pinepark', 'Progreso',
+            'Fraccionamiento Ruta 74', 'Salinas', 'San Antonio', 'San Bautista', 'San Jacinto',
+            'San Luis', 'San Ramón', 'Santa Lucía', 'Santa Rosa', 'Sauce', 'Soca', 'Tala', 'Toledo',
+            'Villa Aeroparque', 'Villa Crespo y San Andrés', 'Villa Felicidad', 'Villa San José'
+        ]
+    ],
+    'CL' => [
+        'label' => 'Cerro Largo',
+        'cities' => [
+            'Melo', 'Aceguá', 'Fraile Muerto', 'Río Branco', 'Arévalo', 'Tupambaé',
+            'Barrio López Benítez', 'Bañado de Medina', 'Cerro de las Cuentas', 'Isidoro Noblía',
+            'Nando', 'Tres Islas', 'Soto', 'Lago Merín'
+        ]
+    ],
+    'CO' => [
+        'label' => 'Colonia',
+        'cities' => [
+            'Colonia del Sacramento', 'Carmelo', 'Juan Lacaze', 'Nueva Palmira', 'Rosario', 'Tarariras',
+            'Nueva Helvecia', 'Ombúes de Lavalle', 'Colonia Valdense', 'Conchillas', 'Colonia Miguelete',
+            'Estanzuela', 'La Paz', 'Florencio Sánchez'
+        ]
+    ],
+    'DU' => [
+        'label' => 'Durazno',
+        'cities' => [
+            'Durazno', 'Sarandí del Yí', 'Villa del Carmen', 'Carlos Reyles', 'Blanquillo',
+            'La Paloma', 'San Jorge', 'Centenario'
+        ]
+    ],
+    'FS' => [
+        'label' => 'Flores',
+        'cities' => [
+            'Trinidad', 'Ismael Cortinas', 'Andresito', 'La Casilla', 'Piedras Coloradas'
+        ]
+    ],
+    'FD' => [
+        'label' => 'Florida',
+        'cities' => [
+            'Florida', 'Sarandí Grande', 'Casupá', 'Fray Marcos', '25 de Mayo', 'La Cruz', 'Reboledo',
+            'Independencia', 'Cardal', 'Chamizo', 'Mendoza', 'Nico Pérez', 'Tala', 'Capilla del Sauce'
+        ]
+    ],
+    'LA' => [
+        'label' => 'Lavalleja',
+        'cities' => [
+            'Minas', 'José Batlle y Ordóñez', 'Mariscala', 'Solís de Mataojo', 'Zapicán', 'Barriga Negra',
+            'Polanco Norte', 'Villa del Rosario'
+        ]
+    ],
+    'MA' => [
+        'label' => 'Maldonado',
+        'cities' => [
+            'Maldonado', 'Punta del Este', 'San Carlos', 'Pan de Azúcar', 'Piriápolis', 'Aiguá', 'Solís',
+            'Garzón', 'José Ignacio', 'Gregorio Aznárez', 'Los Talas', 'Sauce de Portezuelo'
+        ]
+    ],
+    'MO' => [
+        'label' => 'Montevideo',
+        'cities' => [
+            'Montevideo'
+        ]
+    ],
+    'PA' => [
+        'label' => 'Paysandú',
+        'cities' => [
+            'Paysandú', 'Guichón', 'Quebracho', 'Chapicuy', 'Piedra Sola', 'Tambores',
+            'Porvenir', 'Arbolito', 'Constancia', 'Orgoroso'
+        ]
+    ],
+    'RN' => [
+        'label' => 'Río Negro',
+        'cities' => [
+            'Fray Bentos', 'Young', 'Nuevo Berlín', 'San Javier', 'Grecco', 'Bellaco', 'Algorta'
+        ]
+    ],
+    'RV' => [
+        'label' => 'Rivera',
+        'cities' => [
+            'Rivera', 'Tranqueras', 'Vichadero', 'La Pedrera', 'Minas de Corrales', 'Paso Ataques'
+        ]
+    ],
+    'RO' => [
+        'label' => 'Rocha',
+        'cities' => [
+            'Rocha', 'Chuy', 'Castillos', 'La Paloma', 'Lascano', 'Velázquez', 'Cebollatí',
+            'Barra del Chuy', '19 de Abril', 'Aguas Dulces'
+        ]
+    ],
+    'SA' => [
+        'label' => 'Salto',
+        'cities' => [
+            'Salto', 'Constitución', 'Belén', 'Colonia Lavalleja', 'San Antonio', 'Termas del Arapey',
+            'Rincón de Valentín', 'Villa Constitución'
+        ]
+    ],
+    'SJ' => [
+        'label' => 'San José',
+        'cities' => [
+            'San José de Mayo', 'Libertad', 'Ciudad del Plata', 'Ecilda Paullier', 'Rodríguez', 'Capurro',
+            'Raigón', 'Ituzaingó', 'Kiyú-Ordeig'
+        ]
+    ],
+    'SO' => [
+        'label' => 'Soriano',
+        'cities' => [
+            'Mercedes', 'Dolores', 'Cardona', 'José Enrique Rodó', 'Palmitas', 'Risso',
+            'Santa Catalina', 'Egaña'
+        ]
+    ],
+    'TA' => [
+        'label' => 'Tacuarembó',
+        'cities' => [
+            'Tacuarembó', 'Paso de los Toros', 'San Gregorio de Polanco', 'Curtina', 'Achar', 'Rincón del Bonete'
+        ]
+    ],
+    'TT' => [
+        'label' => 'Treinta y Tres',
+        'cities' => [
+            'Treinta y Tres', 'Vergara', 'Santa Clara de Olimar', 'Rincón', 'Cerro Chato', 'Villa Sara'
+        ]
+    ]
+];
